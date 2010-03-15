86bb9ce6b95d5530261e89c3759dc03a29169b4f
Ray Lehtiniemi <rayl@mail.com>
2010-03-14 19:39:59 -0600

    Label all uses if VFS_IMAGE_ABCD
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- src/proto.c
+++ src/proto.c
@@ -774,8 +774,8 @@ static int validity_cycle3 (struct vfs_dev *dev)
 	_(  img_0         (dev, 100));
 	_(  Peek          (dev, VFS_CONTRAST, 0x01));
 	_(  Peek          (dev, 0x00FF500E, 0x02));
-	_(  Peek          (dev, 0x00FF5032, 0x01));
-	_(  Poke          (dev, 0x00FF5032, 0x00000012, 0x01));
+	_(  Peek          (dev, VFS_IMAGE_ABCD, 0x01));
+	_(  Poke          (dev, VFS_IMAGE_ABCD, 0x00000012, 0x01));
 	_(  Poke          (dev, 0x00FF500E, 0x00004000, 0x02));
 	_(  Poke          (dev, VFS_CONTRAST, 0x0000000F, 0x01));
 	_(  SetParam      (dev, 0x0062, 0x0000));
@@ -800,7 +800,7 @@ static int validity_cycle3 (struct vfs_dev *dev)
 	_(  SetParam      (dev, 0x0078, 0x21A0));
 	_(  Poke          (dev, VFS_CONTRAST, 0x00000014, 0x01));
 	_(  Poke          (dev, 0x00FF500E, 0x000021B4, 0x02));
-	_(  Poke          (dev, 0x00FF5032, 0x00000031, 0x01));
+	_(  Poke          (dev, VFS_IMAGE_ABCD, 0x00000031, 0x01));
 	_(  SetParam      (dev, 0x0062, 0x0032));
 	_(  img_abort     (dev));
 	_(  SetParam      (dev, 0x0062, 0x0032));
@@ -878,7 +878,7 @@ static int test_gain (struct vfs_dev *dev)
 {
 	int i;
 	for (i=0; i<0x100; i++) {
-		_(  Poke (dev, 0x00ff5032, i, 0x01));
+		_(  Poke (dev, VFS_IMAGE_ABCD, i, 0x01));
 		_(  img_0 (dev, 100));
 	}
 	return 0;	
