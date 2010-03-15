a5b11d1c59fbe9b982e5dca00f8d75ac662424ed
Ray Lehtiniemi <rayl@mail.com>
2010-03-14 18:41:50 -0600

    Add hex value to image name, to help spot patterns in reg settings
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- src/proto.c
+++ src/proto.c
@@ -173,7 +173,9 @@ static void dump_pnm (struct vfs_dev *dev)
 	char name[40];
 	FILE *pnm;
 
-	sprintf(name, "img/out-%03d.%s", dev->inum++, dev->ilen ? "pnm" : "pnmx");
+	sprintf(name, "img/out-%03d-%02x.%s", dev->inum, dev->inum, dev->ilen ? "pnm" : "pnmx");
+	dev->inum++;
+
 	pnm = fopen(name, "w");
 
 	fprintf(pnm, "P2\n292 %d\n256\n", length / PKTSIZE);
